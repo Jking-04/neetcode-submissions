0837c82929592c3948766bc73d44d349cc896d5f
Jking04 <jamestrking04@gmail.com>
2026-05-05 14:33:40 +0200

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+class TrieNode:
+    def __init__(self):
+        self.end_of_word = False
+        self.children = {}
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for letter in word:
+            if letter in node.children:
+                node = node.children[letter]
+            else:
+                new_node = TrieNode()
+                node.children[letter] = new_node
+                node = new_node
+
+        node.end_of_word = True 
+        
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        return self.searchHelper(word,node)
+        
+    def searchHelper(self,word:str,node:TrieNode) -> bool:
+        for i,letter in enumerate(word):
+            if not letter=='.':
+                if letter in node.children:
+                    node = node.children[letter]
+                else:
+                    return False
+            else:
+                for possible_node in node.children.values():
+                    found = self.searchHelper(word[i+1:],possible_node)
+                    if found:
+                        return True
+                return False
+
+
+        else:
+            return node.end_of_word
+        
