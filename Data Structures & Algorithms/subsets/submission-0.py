db91ad6851ccb653c731b5f61f7e1ed20f55be15
Jking04 <jamestrking04@gmail.com>
2026-05-07 10:42:47 +0200

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.results = [[]]
+        curr_path = []
+        self.find_results(nums,curr_path)
+        print(self.results)
+        return self.results
+    
+    def find_results(self,nums,curr):
+        if nums:
+            for i,next_candidate in enumerate(nums):
+                new_sub_set = curr.copy()
+                new_sub_set.append(next_candidate)
+                if set(new_sub_set) not in [set(sub_set) for sub_set in self.results]: 
+                    self.results.append(new_sub_set)
+                    print(new_sub_set)
+        
+                    self.find_results(nums[:i] + nums[i+1:],new_sub_set)
+
+
+
+        
